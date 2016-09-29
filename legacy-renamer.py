5288dc2d7f648282b3eacd460ab872f1b9d2f032
Edwin <edwinvrooij@gmail.com>
2016-09-29 23:43:46 +0200

    updated naming

---
+++ legacy-renamer.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+import os
+import sys
+
+# def incorrect_syntax(arguments):
+# """
+#     Determines whether or not the user entered a correct syntax
+#     :param arguments: commandline parameter
+#     :return: true or false
+#     """
+#
+#     if len(arguments) is not 1:
+#         return True
+#
+#
+# def rename_files(source_dir, new_name):
+#     """
+#     Searches trough a directory, moves all files with given extension
+#     to target directory.
+#     :return:
+#     """
+#     for root_dir, subdirectories, files in os.walk(source_dir):
+#         # Absolute path of source directory
+#         root_dir_abs = os.path.abspath(root_dir)
+#         print('Checking in root directory {}...'.format(root_dir_abs))
+#
+#         counter = 1
+#
+#         # Loop trough every file in current directory
+#         for file in files:
+#             print('Checking file {}...'.format(file))
+#             old_name = file
+#             filename, file_extension = os.path.splitext(file)
+#            real_new_name = '{}_{}{}'.format(new_name, counter, file_extension)
+#            os.rename(file, real_new_name)
+#            counter += 1
+#            print('Renaming file {} to {}...'.format(old_name, real_new_name))
+
+import os.path
+
+
+def rename_files(paths, new_name):
+    for path in paths:
+        for dirpath, dirnames, filenames in os.walk(path):
+            for filename in filenames:
+                full_path = os.path.join(dirpath, filename)
+                extension = os.path.splitext(filename)[1]
+                print('Would rename {} to {}...'.format(filename, new_name))
+                #os.rename(filename, new_name)
+
+
+def main(args):
+    """Main method run from commandline"""
+    if len(args) is not 2:
+        print(
+            'Syntax error. Correct usage:\npython {} <directory> <new_name>'.format(
+                os.path.basename(__file__)))
+        return
+
+    directory = args[0]
+    new_name = args[1]
+
+    rename_files(directory, new_name)
+
+    print('\n')
+
+
+if __name__ == "__main__":
+    """Python native main method, pass all parameters but the first one, being the name of this script"""
+    main(sys.argv[1:])
--- renamer.py
+++ renamer.py
@@ -1,70 +1,46 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+
+# Import modules
 import os
 import sys
 
-# def incorrect_syntax(arguments):
-# """
-#     Determines whether or not the user entered a correct syntax
-#     :param arguments: commandline parameter
-#     :return: true or false
-#     """
-#
-#     if len(arguments) is not 1:
-#         return True
-#
-#
-# def rename_files(source_dir, new_name):
-#     """
-#     Searches trough a directory, moves all files with given extension
-#     to target directory.
-#     :return:
-#     """
-#     for root_dir, subdirectories, files in os.walk(source_dir):
-#         # Absolute path of source directory
-#         root_dir_abs = os.path.abspath(root_dir)
-#         print('Checking in root directory {}...'.format(root_dir_abs))
-#
-#         counter = 1
-#
-#         # Loop trough every file in current directory
-#         for file in files:
-#             print('Checking file {}...'.format(file))
-#             old_name = file
-#             filename, file_extension = os.path.splitext(file)
-#            real_new_name = '{}_{}{}'.format(new_name, counter, file_extension)
-#            os.rename(file, real_new_name)
-#            counter += 1
-#            print('Renaming file {} to {}...'.format(old_name, real_new_name))
 
-import os.path
+# Start program
+def main():
+    args = sys.argv[1:]
 
-
-def rename_files(paths, new_name):
-    for path in paths:
-        for dirpath, dirnames, filenames in os.walk(path):
-            for filename in filenames:
-                full_path = os.path.join(dirpath, filename)
-                extension = os.path.splitext(filename)[1]
-                print('Would rename {} to {}...'.format(filename, new_name))
-                #os.rename(filename, new_name)
-
-
-def main(args):
-    """Main method run from commandline"""
-    if len(args) is not 2:
-        print(
-            'Syntax error. Correct usage:\npython {} <directory> <new_name>'.format(
-                os.path.basename(__file__)))
+    # Check for valid syntax. We need a file path and new name, if no valid syntax, end program and give correct syntax
+    if len(args) != 2:
+        print("Syntax: remake-renamer.py <filepath> <newname>")
         return
-
-    directory = args[0]
-    new_name = args[1]
-
-    rename_files(directory, new_name)
-
-    print('\n')
+    else:
+        rename_files(args[0], args[1])
 
 
-if __name__ == "__main__":
-    """Python native main method, pass all parameters but the first one, being the name of this script"""
-    main(sys.argv[1:])
+# Loop trough all files in file path and rename, recursively
+def rename_files(source_dir, new_name_raw):
+    # Number to put behind new name
+    index = 0
+
+    for dirpath, dirnames, filenames in os.walk(source_dir):
+        for filename in filenames:
+            full_path = os.path.join(dirpath, filename)
+
+            index += 1
+
+            # Extract extension
+            filename, file_extension = os.path.splitext(full_path)
+
+            # Create new name
+            new_name = new_name_raw + str(index) + file_extension
+
+            # Rename
+            print('Renaming {} to {}'.format(full_path, new_name))
+            os.rename(full_path, new_name)  # Call main method
+
+            # # Loop trough files
+            # for root_dir, subdirectories, files in os.walk(source_dir):
+
+
+if __name__ == '__main__':
+    main()
