6801de855cf1bac51acbcec05f9f07fee7b65162
Edwin <edwinvrooij@gmail.com>
2016-09-29 22:34:37 +0200

    dunno

--- remake-renamer.py
+++ remake-renamer.py
@@ -1,4 +1,17 @@
 import os
 import sys
 
+args = sys.argv
+print(args)
+
+# Receive commandline parameters
+
+
+# Check for valid syntax. We need a file path and new name, if no valid syntax, end program and give correct syntax
+
+
+# Loop trough all files in file path, recursively
+
+    # Rename to have the new name with same file extension
+    # Print the old and new name
 
